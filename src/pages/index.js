f7c6fc1fb4810565db6354c163cdd6a6d0bcd4c9
Ruhani Makhija <ruhani.makhija@brightchamps.com>
2024-07-28 13:01:32 +0530

    added featured posts

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 
 import Layout from "@/layout";
-
+import FeaturedPosts from "@/components/FeaturedPosts";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,8 +16,11 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main >
-        <Layout/>
+      <main>
+        <Layout>
+          {" "}
+          <FeaturedPosts />{" "}
+        </Layout>
       </main>
     </>
   );
